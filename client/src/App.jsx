cea58d1460de063f409681fe31a794c4e7df695a
Huzaifa <76128718+Huzaifa4472@users.noreply.github.com>
2024-04-09 04:48:19 +0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Mainpage from "./page/Mainpage";
-import styled from "styled-components";
-const Container = styled.div`
-  background-color: #f5f5f5;
+// import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+// const Container = styled.div`
+//   background-color: #f5f5f5;
+// `;
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: #f5f5f5; /* Set your desired background color */
+  }
 `;
 function App() {
   return (
-    <Container>
+    <>
+      {" "}
+      <GlobalStyle />
+      {/* <Container> */}
       <BrowserRouter>
         <Navbar />
         <Mainpage />
@@ -15,7 +27,8 @@ function App() {
           <Route></Route>
         </Routes>
       </BrowserRouter>
-    </Container>
+      {/* </Container>{" "} */}
+    </>
   );
 }
 
